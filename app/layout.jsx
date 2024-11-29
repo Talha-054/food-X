3dc0a7fb8034140651aa797cfe0be6136147a7da
Muhammad Talha <03345238002h@gmail.com>
2024-11-29 18:33:56 +0500

    ..

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,24 @@
-import Header from "@/components/header/Header"
-import "./globals.css"
-import Footer from "@/components/footer/Footer"
+import Header from "@/components/header/Header";
+import "./globals.css";
+import Footer from "@/components/footer/Footer";
+
+export const metadata = {
+  title: "Indische Tandoori, indian food in bremerhaven",
+  description:
+    "Willkommen im Indische Tandoori Indian Restaurant! Genießen Sie traditionelle nordindische Küche mit exotischen Zutaten, duftenden Currys und intensiven Gewürzen. Jetzt Tisch reservieren oder online bestellen.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="./favicon.ico" />
+      </head>
       <body className="">
-        {/* <Header /> */}
         {children}
         <Footer />
       </body>
+      
     </html>
-  )
+  );
 }
--- components/footer/FooterDetails.jsx
+++ components/footer/FooterDetails.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
@@ -9,96 +9,159 @@ import { FaLinkedinIn } from "react-icons/fa6";
 import { IoLocationSharp } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import footerBorderImg from "../../public/assets/footer-border.png"
+import { motion } from "framer-motion";
+import Image from "next/image";
+import footerBorderImg from "../../public/assets/footer-border.png";
+import Link from "next/link";
 
 const aboutUsData = [
-    {name: "youtube", logo: <FaYoutube size={15} color='white'/>},
-    {name: "facebook", logo: <FaFacebookF size={15} color='white'/>},
-    {name: "linkedIn", logo: <FaLinkedinIn size={15} color='white'/>},
-    {name: "X", logo: <FaTwitter size={15} color='white'/>},
-]
+  { name: "youtube", logo: <FaYoutube size={15} color="white" /> },
+  { name: "facebook", logo: <FaFacebookF size={15} color="white" /> },
+  { name: "linkedIn", logo: <FaLinkedinIn size={15} color="white" /> },
+  { name: "X", logo: <FaTwitter size={15} color="white" /> },
+];
 
-  // contact info
+// contact info
 
-  const contactData = [
-    {name: "location", text: "Ratiborerstr. 4027578 Bremerhaven", icon: <IoLocationSharp size={20} color='white'/>},
-    {name: "cell-number", text: "+49(0)47196157933", icon: <FaPhone size={20} color='white'/>},
-    {name: "email", text: "info(at)thaya-restaurant.de", icon: <MdEmail size={20} color='white'/>},
-]
-
+const contactData = [
+  {
+    name: "location",
+    text: "Ratiborerstr. 4027578 Bremerhaven",
+    icon: <IoLocationSharp size={20} color="white" />,
+  },
+  {
+    name: "cell-number",
+    text: "+49(0)47196157933",
+    icon: <FaPhone size={20} color="white" />,
+  },
+  {
+    name: "email",
+    text: "info(at)thaya-restaurant.de",
+    icon: <MdEmail size={20} color="white" />,
+  },
+];
 
 export default function FooterDetails() {
   return (
     <>
-         <motion.div
-         initial={{y:200, opacity:0}}
-         whileInView={{y:125, opacity:1}}
-         transition={{duration:0.75, type:"spring"}}
-          id='container-1' 
-          className='bg-[#2e2d2d] flex p-[1em] md:w-[85vw] md:p-[2em] relative lg:translate-y-[30%] z-10'>
-            <div className='flex w-full flex-col xl:flex-row xl:justify-evenly justify-start items-start gap-12 xl:gap-0 '>
-              <div className="section-1 w-full flex flex-col md:flex-row md:justify-evenly md:gap-0 gap-10">
-                <div className='md:border-r-[1px] border-[F8F9FA] border-opacity-50 md:pr-[1em]'>
-                  <h2 className='text-[24px] text-[#FFFFFF] font-body font-semibold'>Über uns</h2>
-                  <p className='text-[16px] text-[#CCCCCC] font-body font-normal my-8 md:max-w-[220px]'>
-                  Von der Vorspeise bis zum Dessert – lassen Sie sich von der Vielfalt der indischen Küche und ihren einzigartigen Gewürzen verzaubern.
-                  </p>
-                  <div className='flex gap-2'>
-                    {aboutUsData.map((record, index)=>{
-                      return (
-                        <span  className="w-[40px] h-[40px] bg-[#3F3F3F] flex justify-center items-center" key={record.name}>{record.logo}</span>
-                      )
-                    })}
-                  </div>
-                </div>
-                <div>
-                  <h2 className='text-[24px] text-[#FFFFFF] font-body font-semibold mb-8'>Entdecken Sie</h2>
-                  <div className='flex flex-col gap-4'>
-                    <p className='text-[16px] font-body text-[#CCCCCC] cursor-pointer'>Über uns</p>
-                    <p className='text-[16px] font-body text-[#CCCCCC]  cursor-pointer'>Kontakt</p>
-                    <p className='text-[16px] font-body text-[#CCCCCC]  cursor-pointer'>Speisekarte</p>
-                    <p className='text-[16px] font-body text-[#CCCCCC]  cursor-pointer' >Galerie</p>
-                    <p className='text-[16px] font-body text-[#CCCCCC]  cursor-pointer'>Reservierung</p>
-                  </div>
-                </div>
+      <motion.div
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ y: 125, opacity: 1 }}
+        transition={{ duration: 0.75, type: "spring" }}
+        id="container-1"
+        className="bg-[#2e2d2d] flex p-[1em] md:w-[85vw] md:p-[2em] relative lg:translate-y-[30%] z-10"
+      >
+        <div className="flex w-full flex-col xl:flex-row xl:justify-evenly justify-start items-start gap-12 xl:gap-0 ">
+          <div className="section-1 w-full flex flex-col md:flex-row md:justify-evenly md:gap-0 gap-10">
+            <div className="md:border-r-[1px] border-[F8F9FA] border-opacity-50 md:pr-[1em]">
+              <h2 className="text-[24px] text-[#FFFFFF] font-body font-semibold">
+                Über uns
+              </h2>
+              <p className="text-[16px] text-[#CCCCCC] font-body font-normal my-8 md:max-w-[220px]">
+                Von der Vorspeise bis zum Dessert – lassen Sie sich von der
+                Vielfalt der indischen Küche und ihren einzigartigen Gewürzen
+                verzaubern.
+              </p>
+              <div className="flex gap-2">
+                {aboutUsData.map((record, index) => {
+                  return (
+                    <span
+                      className="w-[40px] h-[40px] bg-[#3F3F3F] flex justify-center items-center"
+                      key={record.name}
+                    >
+                      {record.logo}
+                    </span>
+                  );
+                })}
               </div>
+            </div>
+            <div>
+              <h2 className="text-[24px] text-[#FFFFFF] font-body font-semibold mb-8">
+                Entdecken Sie
+              </h2>
+              <div className="flex flex-col gap-4">
+                <p className="text-[16px] font-body text-[#CCCCCC] cursor-pointer">
+                  Über uns
+                </p>
+                <p className="text-[16px] font-body text-[#CCCCCC]  cursor-pointer">
+                  Kontakt
+                </p>
+                <p className="text-[16px] font-body text-[#CCCCCC]  cursor-pointer">
+                  Speisekarte
+                </p>
+                <p className="text-[16px] font-body text-[#CCCCCC]  cursor-pointer">
+                  Galerie
+                </p>
+                <p className="text-[16px] font-body text-[#CCCCCC]  cursor-pointer">
+                  Reservierung
+                </p>
+              </div>
+            </div>
+          </div>
 
-
-              <div className="section-2 w-full flex flex-col md:flex-row md:justify-evenly md:gap-0 md:pl-[4em] xl:pl-0  gap-10">
-                <div>
-                  <h2 className='text-[24px] text-[#FFFFFF] font-body font-semibold mb-8'>Kontaktinformationen.</h2>
-                  <div className='flex flex-col gap-6'>
-                    {contactData.map((record)=>{
-                      return (
-                        <div className='flex gap-4 justify-start items-center' key={record.name}>
-                          <span className='bg-[#760124] h-[35px] w-[35px] flex justify-center items-center'>{record.icon}</span>
-                          <p className='text-[16px] text-[#FFFFFF] font-medium font-body md:max-w-[200px]'>{record.text}</p>
-                        </div>
-                      )
-                    })}
-                  </div>
-                </div>
-
-                <div className=''>
-                  <h2 className='text-[24px] text-[#FFFFFF] font-body font-semibold mb-8'>Newsletter</h2>
-                  <p className='text-[16px] text-[#CCCCCC] font-body font-normal my-8 max-w-[250px]'>
-                  Treten Sie unserer Abonnentenliste bei, um die neuesten Nachrichten und Sonderangebote zu erhalten.
-                  </p>
-                  <label htmlFor='email' className='relative'>
-                    <input placeholder='Ihre E-Mail.' className='w-full md:w-[250px] text-[#FFFFFF] font-body px-2 focus:outline-none h-[50px] bg-[#2e2d2d] border-b-[1px] border-[#F8F9FA] border-opacity-50' type="text" name="" id="email" />
-                    <span className='h-[50px] w-[50px] bg-[#2e2d2d] z-10 flex justify-center items-center absolute top-0 -translate-y-[30%] right-0'><FaArrowRight color='white' size={15}/></span>
-                  </label>
-                  
-                </div>
+          <div className="section-2 w-full flex flex-col md:flex-row md:justify-evenly md:gap-0 md:pl-[4em] xl:pl-0  gap-10">
+            <div>
+              <h2 className="text-[24px] text-[#FFFFFF] font-body font-semibold mb-8">
+                Kontaktinformationen.
+              </h2>
+              <div className="flex flex-col gap-6">
+                {contactData.map((record) => {
+                  return (
+                    <div
+                      className="flex gap-4 justify-start items-center"
+                      key={record.name}
+                    >
+                      <span className="bg-[#760124] h-[35px] w-[35px] flex justify-center items-center">
+                        {record.icon}
+                      </span>
+                      <p className="text-[16px] text-[#FFFFFF] font-medium font-body md:max-w-[200px]">
+                        {record.text}
+                      </p>
+                    </div>
+                  );
+                })}
               </div>
             </div>
 
-            <div id="footer-top-border" className='absolute hidden xl:block right-0 top-[-7%] w-[85vw] h-[30px] z-20 '>
-              <Image alt='footer background image' src={footerBorderImg} className='absolute w-full h-full'/>
+            <div className="">
+              <Link href={"/impressum"}>
+                <h2 className="text-[24px] text-[#FFFFFF] font-body font-semibold mb-8 hover:cursor-pointer">
+                  Impressum
+                </h2>
+              </Link>
+
+              <div className="flex flex-col  gap-2">
+                <Link href={"/AGB"}>
+                  <p className="text-[16px] text-[#CCCCCC] font-body font-medium hover:cursor-pointer">
+                    AGB
+                  </p>
+                </Link>
+                <Link href={"/datenschutz"}>
+                  <p className="text-[16px] text-[#CCCCCC] font-body font-medium hover:cursor-pointer">
+                    Datenschutz
+                  </p>
+                </Link>
+                <Link href={"/zahlungsweisen"}>
+                  <p className="text-[16px] text-[#CCCCCC] font-body font-medium hover:cursor-pointer">
+                    Zahlungsweisen
+                  </p>
+                </Link>
+              </div>
             </div>
-          </motion.div>
+          </div>
+        </div>
+
+        <div
+          id="footer-top-border"
+          className="absolute hidden xl:block right-0 top-[-7%] w-[85vw] h-[30px] z-20 "
+        >
+          <Image
+            alt="footer background image"
+            src={footerBorderImg}
+            className="absolute w-full h-full"
+          />
+        </div>
+      </motion.div>
     </>
-  )
+  );
 }
